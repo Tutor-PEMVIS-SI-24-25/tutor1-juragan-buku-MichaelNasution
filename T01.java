4e879ecb01b94a039a153810f86f0619191d6150
Michael Pratama Nasution <100076526+striktrr@users.noreply.github.com>
2024-09-04 21:27:10 +0700

    Telah Menyelesaikan Tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,13 @@
-// NIM - NAMA
-// NIM - NAMA
+// NIM - Michael
+// NIM - Mutiara
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    public static void main(String[] args) {
+        String iSBN;
+        String judul;
+
+    }
+}
